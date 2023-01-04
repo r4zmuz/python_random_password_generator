9a3df908e8eb8b4f697f9ce0c715c1995d81d087
r4zmuz <rasmus.spiegel@usa.com>
2023-01-04 14:34:24 +0200

    first version finished code

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
-# This is a sample Python script.
+import random
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+#https://www.101computing.net/wp/wp-content/uploads/ASCII-Table.pdf (ASCII table)
+
+uppercaseLetter1 = chr(random.randint(65, 90))  # Generate a random Uppercase letter (based on ASCII code)
+uppercaseLetter2 = chr(random.randint(65, 90)) # Generate a random Uppercase letter (based on ASCII code)
+lowercaseLetter1 = chr(random.randint(97, 122))  # Generate a random lowercase letter (based on ASCII code)
+lowercaseLetter2 = chr(random.randint(97, 122))  # Generate a random lowercase letter (based on ASCII code)
+digit1 = chr(random.randint(48, 57))  # Generate a random number (based on ASCII code)
+digit2 = chr(random.randint(48, 57))  # Generate a random number letter (based on ASCII code)
+punctuationSign1 = chr(random.randint(33, 47))  # Generate a random punctuation sign (based on ASCII code)
+punctuationSign2 = chr(random.randint(33, 47))  # Generate a random punctuation sign (based on ASCII code)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# Generate password using all the characters, in random order
+password = uppercaseLetter1 + uppercaseLetter2 + lowercaseLetter1 + \
+           lowercaseLetter2 + digit1 + digit2 + punctuationSign1 + \
+           punctuationSign2
 
 
-# Press the green button in the gutter to run the script.
+# A function do shuffle all the characters of a string
+def shuffle(password):
+    password = list(password)
+    random.shuffle(password)
+    return ''.join(password)
+
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print(password)
