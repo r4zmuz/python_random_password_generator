f2c69d324ab5714f1d74e911b7f0d554e9c689e8
r4zmuz <rasmus.spiegel@usa.com>
2023-01-06 22:49:01 +0200

    added clipboard option

--- GUI.py
+++ GUI.py
@@ -1,6 +1,7 @@
 import random
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QWidget, QLabel, QLineEdit, QPushButton
+from PyQt5.QtWidgets import QWidget, QLabel, QPushButton
+import pyperclip
 
 class MainWindow(QWidget):
     def __init__(self):
@@ -10,21 +11,25 @@ class MainWindow(QWidget):
 
         # Create the widgets
         self.setGeometry(100, 100, 600, 100) #set window size
-        self.label = QLabel("Generate Password", self)
-        self.line_edit = QLineEdit(self)
+        self.label = QLabel("Generate Password:", self)
+        self.gen_label = QLabel(self)
+        self.gen_label.hide()
         self.generate_button = QPushButton("Generate", self)
+        self.copy_button = QPushButton("Copy to Clipboard", self)
         self.clean_button = QPushButton("Clean", self)
 
         # Set up the layout
         layout = QtWidgets.QVBoxLayout(self)
         layout.addWidget(self.label)
-        layout.addWidget(self.line_edit)
+        layout.addWidget(self.gen_label)
         layout.addWidget(self.generate_button)
+        layout.addWidget(self.copy_button)
         layout.addWidget(self.clean_button)
         self.setLayout(layout)
 
         # Connect the buttons to the slots
         self.generate_button.clicked.connect(self.generate)
+        self.copy_button.clicked.connect(self.copy)
         self.clean_button.clicked.connect(self.clean)
 
     def generate(self):
@@ -48,8 +53,8 @@ class MainWindow(QWidget):
 
         # Generate password using all the characters, in random order
         self.password = uppercaseLetter1 + uppercaseLetter2 + lowercaseLetter1 + \
-                   lowercaseLetter2 + digit1 + digit2 + punctuationSign1 + \
-                   punctuationSign2
+                        lowercaseLetter2 + digit1 + digit2 + punctuationSign1 + \
+                        punctuationSign2
 
         def shuffle(password):
             password = list(password)
@@ -57,11 +62,20 @@ class MainWindow(QWidget):
             return ''.join(password)
 
         # Shuffle the characters in the password
-        gen_password = shuffle(self.password)
-        self.label.setText("Generated Password:")
-        self.line_edit.setText(gen_password)
+        self.gen_password = shuffle(self.password)
+        self.label.setText("Generated Password: ")
+        self.gen_label.setText(self.gen_password)
+        #show gen_label, creates new line for generated password
+        self.gen_label.show()
+
 
     def clean(self):
-        self.password = ""
         self.label.setText("Generate Password:")
-        self.line_edit.setText(self.password)
+        password = ""
+        self.gen_label.setText(password)
+        self.gen_label.hide()
+
+    def copy(self):
+        copy_password = self.gen_password
+        #to copy generated password to clipboard
+        pyperclip.copy(copy_password)
