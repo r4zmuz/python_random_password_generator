a86a5581b7d968e99fcfe709de331f901ffaf22f
r4zmuz <rasmus.spiegel@usa.com>
2023-02-18 18:14:09 +0200

    updated password generator

--- src/password_generator/passwordGenerator.py
+++ src/password_generator/passwordGenerator.py
@@ -4,26 +4,26 @@ def generate() -> str:
     # https://www.101computing.net/wp/wp-content/uploads/ASCII-Table.pdf (ASCII table)
     # Generate a random Uppercase letter (based on ASCII code)
     uppercaseLetter1 = chr(random.randint(65, 90))
-    # Generate a random Uppercase letter (based on ASCII code)
     uppercaseLetter2 = chr(random.randint(65, 90))
+    uppercaseLetter3 = chr(random.randint(65, 90))
     # Generate a random lowercase letter (based on ASCII code)
     lowercaseLetter1 = chr(random.randint(97, 122))
-    # Generate a random lowercase letter (based on ASCII code)
     lowercaseLetter2 = chr(random.randint(97, 122))
+    lowercaseLetter3 = chr(random.randint(97, 122))
     # Generate a random number (based on ASCII code)
     digit1 = chr(random.randint(48, 57))
-    # Generate a random number letter (based on ASCII code)
     digit2 = chr(random.randint(48, 57))
+    digit3 = chr(random.randint(48, 57))
     # Generate a random punctuation sign (based on ASCII code)
     punctuationSign1 = chr(random.randint(33, 47))
-    # Generate a random punctuation sign (based on ASCII code)
     punctuationSign2 = chr(random.randint(33, 47))
+    punctuationSign3 = chr(random.randint(33, 47))
 
     # Generate password using all the characters, in random order
-    password = uppercaseLetter1 + uppercaseLetter2 + lowercaseLetter1 + \
-                    lowercaseLetter2 + digit1 + digit2 + punctuationSign1 + \
-                    punctuationSign2
-    # Shuffle password
+    password = uppercaseLetter1 + uppercaseLetter2 + uppercaseLetter3 + lowercaseLetter1 + \
+                    lowercaseLetter2 + lowercaseLetter3 + digit1 + digit2 + digit3 + punctuationSign1 + \
+                    punctuationSign2 + punctuationSign3
+    # Shuffle random characters
     password_lst = list(password)
     random.shuffle(password_lst)
     return  ''.join(password_lst)
